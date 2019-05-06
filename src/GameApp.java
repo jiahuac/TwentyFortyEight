fb7cb15dc5681d9c641220ac9e25d3ac94bb5306
Jiahua Chen <jc@jh.tc>
2019-05-05 22:41:59 -0400

    Screens

--- src/GameApp.java
+++ src/GameApp.java
@@ -20,26 +20,65 @@ public class GameApp {
     /** the window of the app */
     private JFrame myWindow;
 
+    private JPanel myPanel;
+
+    private static final String TITLE = "Title";
+    private static final String GAME = "Game";
+    private static final String INSTRUCTION = "Instruction";
+
+
     /** the screen of the app */
-    private GameScreen myScreen;
+    private GameScreen myGameScreen;
+    private TitleScreen myTitleScreen;
+    private InstructionScreen myInstructionScreen;
+
+    private Grid gameGrid;
 
     public GameApp()
     {
-        Grid gameGrid = new Grid();
-        myScreen = new GameScreen(gameGrid);
+        myTitleScreen = new TitleScreen(this);
+        gameGrid = new Grid();
+        myGameScreen = new GameScreen(gameGrid);
+        myInstructionScreen = new InstructionScreen(this);
+    }
 
+    public void run()
+    {
         myWindow = new JFrame();
+
         myWindow.setSize(WIDTH, HEIGHT);
         myWindow.setTitle("2048^3");
         myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        myWindow.add(myScreen);
+        myPanel = new JPanel(new CardLayout());
 
+        myPanel.add(myGameScreen, GAME);
+        myPanel.add(myTitleScreen, TITLE);
+        myPanel.add(myInstructionScreen, INSTRUCTION);
+        CardLayout layout = (CardLayout) myPanel.getLayout();
+        layout.show(myPanel, TITLE);
+
+        myWindow.add(myPanel);
         myWindow.setVisible(true);
+    }
+
+    public void loadGameScreen()
+    {
+        CardLayout layout = (CardLayout) myPanel.getLayout();
+        layout.show(myPanel, GAME);
+        myGameScreen.requestFocusInWindow();
+    }
+
+    public void loadInstructionScreen()
+    {
+        CardLayout layout = (CardLayout) myPanel.getLayout();
+        layout.show(myPanel, INSTRUCTION);
+        myInstructionScreen.requestFocusInWindow();
     }
 
     public static void main(String[] args)
     {
         GameApp main = new GameApp();
+        main.run();
     }
 }
